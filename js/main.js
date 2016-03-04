58df128f398825ceb91dcd480eb290643a13cfc5
Steve Chaloner <bitbucket@objectify.be>
2016-03-04 17:10:34 +0100

    new website

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	 $("#slide-testimonial").owlCarousel({
+		 
+		autoPlay: 4000, 
+		 
+		singleItem:true,
+		pagination: true,
+		 
+	});
+	$('#menu').slicknav();
+});
